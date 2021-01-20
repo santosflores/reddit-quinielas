362974955647efe94ed5dabc735559448da8d390
Santos Flores <santosflores@gmail.com>
2021-01-19 19:25:09 -0800

    Added animations to highligth the change between rounds

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,33 @@
-import { NONE_TYPE } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { sheets_v4 } from 'googleapis';
-import { LeaderboardComponent } from '../leaderboard/leaderboard.component';
 import { SheetService, Leaderboard, Result } from '../sheet.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations: [// ...
+    trigger('openClose', [
+      state('open', style({
+        opacity: 1
+      })),
+      state('closed', style({
+        opacity: 0        
+
+      })),
+      transition('open <=> closed', [
+        animate('1s')
+      ]),
+    ]),
+  ]
 })
 export class MainComponent implements OnInit {
   dataSource?: sheets_v4.Schema$ValueRange;
@@ -20,6 +40,11 @@ export class MainComponent implements OnInit {
   winner?: Result;
   loser?: Result;
   matchday: number = 1;
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 
   constructor(private sheet: SheetService) {
     this.sheet.getData().subscribe((d: sheets_v4.Schema$ValueRange) => {
@@ -37,16 +62,23 @@ export class MainComponent implements OnInit {
     });
   }
 
-  updateRound(matchday: number) {
+  async updateRound(matchday: number) {
+    this.toggle();
+    await this.sleep(500);
     this.matchday = matchday;
     this.round = this.sheet.getRoundIndex(this.leaderboard, this.matchday);
     this.topRound = this.leaderboard?.rounds[this.round].results.sort(this.sheet.sortDesc).slice(0, 5);
     this.winner = this.topRound?.shift();
     this.wallOfShameRound = this.leaderboard?.rounds[this.round].results.sort(this.sheet.sortAsc).slice(0, 5);
     this.loser = this.wallOfShameRound?.shift();
+    this.toggle();
   }
 
   ngOnInit(): void {
+  }
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
